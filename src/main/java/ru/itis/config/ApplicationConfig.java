4af7c00396abe56e1e7930e37dc7fa829b42a613
azaliyas-github <1maxovmax1@gmail.com>
2021-05-09 16:35:05 +0300

    Created project

---
+++ src/main/java/ru/itis/config/ApplicationConfig.java
@@ -0,0 +1,8 @@
+package ru.itis.config;
+
+import org.springframework.context.annotation.*;
+
+@Configuration
+public class ApplicationConfig {
+
+}
---
+++ src/main/java/ru/itis/controllers/MainPageController.java
@@ -0,0 +1,13 @@
+package ru.itis.controllers;
+
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class MainPageController {
+
+    @GetMapping("/main")
+    public String mainPage() {
+        return "main";
+    }
+}
