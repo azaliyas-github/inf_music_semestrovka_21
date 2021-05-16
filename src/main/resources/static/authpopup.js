90a9577709b6840b82a75454db3fdc5a8ae8727e
azaliyas-github <1maxovmax1@gmail.com>
2021-05-16 04:05:27 +0300

    Added jQuery

--- src/main/resources/static/authpopup.js
+++ src/main/resources/static/authpopup.js
@@ -1,29 +1,5 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container-modal');
-
-signUpButton.addEventListener('click', () => {
-    container.classList.add("right-panel-active");
-});
-
-signInButton.addEventListener('click', () => {
-    container.classList.remove("right-panel-active");
-});
-
-// код открытия попапа по клику на профиль
-$(".js-show-modal").on("click", function(event) {
-    event.preventDefault();
-
-    $(".js-modal, #js-overlay").fadeIn();
-
-    $("body").addClass("open-modal");
-});
-
-// код закрытия попапа
-$(".js-modal-close").on("click", function(event) {
-    event.preventDefault();
-
-    $(".js-modal, #js-overlay").fadeOut();
-
-    $("body").removeClass("open-modal");
+$(function () {
+    const container = $("#container-modal");
+    $("#signUp").on("click", function () { container.addClass("right-panel-active"); });
+    $("#signIn").on("click", function () { container.removeClass("right-panel-active"); });
 });
