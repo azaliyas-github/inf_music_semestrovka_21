435e6f8cad1cddcc5b5ef388742de09cad14bdac
azaliyas-github <1maxovmax1@gmail.com>
2021-05-29 23:12:26 +0300

    Deleted unused page templates

--- src/main/resources/static/authpopup.js
+++
@@ -1,5 +0,0 @@
-$(function () {
-    const container = $("#container-modal");
-    $("#signUp").on("click", function () { container.addClass("right-panel-active"); });
-    $("#signIn").on("click", function () { container.removeClass("right-panel-active"); });
-});
