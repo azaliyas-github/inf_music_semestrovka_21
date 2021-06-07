df19cd6a737a9f4a9dc4aab5d1a4aac0936fd0c8
azaliyas-github <1maxovmax1@gmail.com>
2021-06-07 20:40:48 +0300

    Implemented message sending

--- src/main/resources/static/layout/scripts/chat.js
+++ src/main/resources/static/layout/scripts/chat.js
@@ -1,6 +1,18 @@
 $(function() {
-	let selectedRecipient;
 	const chatWindow = $(".chat-window");
+	const sockJsClient = new SockJS("/chat");
+	const stompClient = Stomp.over(sockJsClient);
+
+	function onStompClientConnected() {
+		stompClient.subscribe("/chat/users/" + currentUserId + "/messages");
+		chatWindow.removeClass("not-connected");
+	}
+	function onStompClientConnectionError() {
+		chatWindow.addClass("not-connected");
+	}
+	stompClient.connect({}, onStompClientConnected, onStompClientConnectionError);
+
+	let selectedRecipient;
 	const selectedRecipientName = $("#selected-recipient-name");
 	const selectedRecipientPhoto = $("#selected-recipient-photo");
 	const baseImageSrc = selectedRecipientPhoto.attr("basesrc");
@@ -14,5 +26,19 @@ $(function() {
 				selectedRecipientPhoto.attr("src", baseImageSrc + recipient.photoFileName);
 				chatWindow.addClass("recipient-selected");
 			});
+	});
+
+	const messageContent = $(".message-content");
+	$(".send-button").click(function(event) {
+		event.preventDefault();
+
+		const message = {
+			senderId: currentUserId,
+			recipientId: selectedRecipient.id,
+			content: messageContent.val()
+		};
+		stompClient.send("/chat/send", {}, JSON.stringify(message));
+
+		messageContent.val("");
 	});
 });
