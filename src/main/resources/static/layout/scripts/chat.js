571d9a1de959119a153404dd59429cc247b5755e
azaliyas-github <1maxovmax1@gmail.com>
2021-06-07 21:28:55 +0300

    Implemented message receiving

--- src/main/resources/static/layout/scripts/chat.js
+++ src/main/resources/static/layout/scripts/chat.js
@@ -3,8 +3,36 @@ $(function() {
 	const sockJsClient = new SockJS("/chat");
 	const stompClient = Stomp.over(sockJsClient);
 
+	const chat = chatWindow.find("#chat");
+	const messagePrototype = chat.find("li.prototype");
+	function addMessageFromUs(message) {
+		addMessage(message, "me");
+	}
+	function addMessageFromThem(message) {
+		addMessage(message, "you");
+	}
+	function addMessage(message, className) {
+		const messageElement = messagePrototype.clone();
+		messageElement.removeClass("prototype");
+		messageElement.addClass(className);
+
+		messageElement.find(".author-name").text(message.senderName);
+		messageElement.find(".message").text(message.content);
+		messageElement.find(".message-datetime").text(
+			new Date(message.creationTimestamp).toLocaleString());
+
+		messageElement.appendTo(chat);
+	}
+
+	function onMessageReceived(stompMessage) {
+		const message = JSON.parse(stompMessage.body);
+		if (message.senderId === currentUserId)
+			addMessageFromUs(message);
+		else
+			addMessageFromThem(message);
+	}
 	function onStompClientConnected() {
-		stompClient.subscribe("/chat/users/" + currentUserId + "/messages");
+		stompClient.subscribe("/chat/users/" + currentUserId + "/messages", onMessageReceived);
 		chatWindow.removeClass("not-connected");
 	}
 	function onStompClientConnectionError() {
